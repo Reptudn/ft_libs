3f1b123972698d93148ec1d1c391d12e127414ea
Jonas Kauker <jkauker@2-E-8.42heilbronn.de>
2023-10-10 15:56:05 +0200

    fixed strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 01:37:45 by jonask            #+#    #+#             */
-/*   Updated: 2023/10/10 13:43:11 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/10/10 15:51:07 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,16 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	total;
 
 	i = 0;
-	if (size == 0)
-		return (ft_strlen(dst));
-	while (src[i] != 0 && i < size - 1)
+	while (src[i] != 0 && (i + 1 < size))
 	{
-		if (src[i] == 0)
-			break ;
 		dst[i] = src[i];
 		i++;
 	}
-	dst[i] = 0;
-	total = ft_strlen(src);
-	return (total);
+	if (i < size)
+		dst[i] = 0;
+	while (src[i])
+		i++;
+	return (i);
 }
