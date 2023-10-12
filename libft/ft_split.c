230f00753dc393608a44fa68a8833911c86e51fa
Jonas Kauker <jkauker@3-F-6.42heilbronn.de>
2023-10-12 14:39:23 +0200

    safety push

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 12:39:55 by jonask            #+#    #+#             */
-/*   Updated: 2023/10/12 13:37:21 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/10/12 14:39:00 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static int	get_word_count(const char *s, char splitter)
 		}
 		count++;
 	}
-	return (words + 1);
+	return (words);
 }
 
 static void	cleanup(char **strs, int allocated_word_cnt)
@@ -49,23 +49,18 @@ static int	get_word_len(char *word, char splitter)
 	return (i);
 }
 
-static int	the_split(char c, const char *s, char **strs)
+static int	the_split(char c, const char *s, char **strs, int i)
 {
-	int	i;
 	int	j;
 	int	curr_word_len;
 	int	str_index;
 
-	i = 0;
 	str_index = 0;
 	while (s[i] != 0)
 	{
-		curr_word_len = 0;
-		while (s[i] != 0 && s[i] == c)
+		while (s[i] == c)
 			i++;
 		curr_word_len = get_word_len((char *)&s[i], c);
-		if (s[i + curr_word_len] == 0)
-			break ;
 		strs[str_index] = malloc(sizeof(char) * (curr_word_len + 1));
 		if (!strs[str_index])
 		{
@@ -78,18 +73,26 @@ static int	the_split(char c, const char *s, char **strs)
 		strs[str_index][j] = 0;
 		str_index++;
 	}
-	strs[str_index] = 0;
+	if (&s[i] != ft_strrchr(&s[i], c))
+		ft_strlcpy(strs[str_index], &s[i], get_word_len((char *)&s[i], c));
 	return (1);
 }
 
 char	**ft_split(const char *s, char c)
 {
 	char	**strs;
+	int		i;
 
 	strs = malloc((get_word_count(s, c)) * sizeof(char *));
 	if (!strs)
 		return (0);
-	if (!the_split(c, s, strs))
+	if (ft_strchr(s, c) == 0)
+	{
+		*strs = ft_strdup(s);
+		return (strs);
+	}
+	i = 0;
+	if (!the_split(c, s, strs, i))
 		return (0);
 	return (strs);
 }
