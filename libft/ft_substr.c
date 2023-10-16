4450f6187ad06a9352ac7ec6fb3ace85879aafce
Jonas Kauker <jkauker@2-G-4.42heilbronn.de>
2023-10-16 10:34:06 +0200

    fixed substring

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 17:14:09 by jonask            #+#    #+#             */
-/*   Updated: 2023/10/13 12:37:23 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/10/16 10:33:54 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,17 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*sub;
 
-	if (start >= ft_strlen(s) || (start == 0 && len == 0))
-		return (ft_strdup(""));
 	if (!s)
 		return (0);
+	if (start >= ft_strlen(s) || (start == 0 && len == 0))
+		return (ft_strdup(""));
+	if (start == 0 && len >= ft_strlen(s))
+		return (ft_strdup(s));
 	sub = malloc((len + 1) * sizeof(char));
-	if (len > ft_strlen(s))
-		len = ft_strlen(s) - start;
 	if (!sub)
 		return (0);
+	if (len > ft_strlen(s))
+		len = ft_strlen(s) - start;
 	makesub(sub, s + start, len + 1);
 	sub[len + 1] = 0;
 	return (sub);
