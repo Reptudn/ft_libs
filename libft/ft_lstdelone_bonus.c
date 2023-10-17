f3b4e3ee8de5393999afeabc566ddb920ca5755c
Jonas Kauker <jkauker@2-A-4.42heilbronn.de>
2023-10-17 09:49:59 +0200

    passing all normal paco libft tests

--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 07:37:43 by jonask            #+#    #+#             */
-/*   Updated: 2023/10/16 12:38:15 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/10/17 09:46:41 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (lst && del)
-	{
-		del(lst->content);
-		free(lst);
-	}
+	if (lst == 0 || del == 0)
+		return ;
+	del(lst->content);
+	free(lst);
 }
--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 07:42:38 by jonask            #+#    #+#             */
-/*   Updated: 2023/10/16 16:41:40 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/10/17 09:47:32 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	pog = 0;
 	i = lst;
+	if (!lst || !f || !del)
+		return (0);
 	while (i != 0)
 	{
 		f_ed_content = f(i->content);
 		new_item = ft_lstnew(f_ed_content);
 		if (new_item == 0)
 		{
-			del(f_ed_content);
 			ft_lstdelone(new_item, del);
+			del(f_ed_content);
 			ft_lstclear(&lst, del);
 			return (0);
 		}
 		ft_lstadd_back(&pog, new_item);
 		i = i->next;
 	}
+	ft_lstadd_back(&pog, 0);
 	return (pog);
 }
