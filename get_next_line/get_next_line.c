b513730f8a68a331617aa037316b666c820a9eaa
Jonas Kauker <jkauker@2-H-4.42heilbronn.de>
2023-11-02 12:24:59 +0100

    lol

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 21:26:38 by jonask            #+#    #+#             */
-/*   Updated: 2023/11/02 11:02:40 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/11/02 12:24:07 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,10 @@ char	*get_next_line(int fd)
 	if (fd == FD_ERR || BUFFER_SIZE < 1 || read(fd, 0, 0) == -1)
 		return (0);
 	if (ft_read(fd, &left) == 0)
+	{
+		//left[BUFFER_SIZE + 1] = 0;
 		return (left);
+	}
 	len = ft_strchr(left, '\n') - left + 1;
 	tmp = malloc((len + 1) * sizeof(char));
 	if (!tmp)
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,23 +6,58 @@
 /*   By: jkauker <jkauker@student.42heilbrnn.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 14:42:21 by jkauker           #+#    #+#             */
-/*   Updated: 2023/11/02 10:58:41 by jkauker          ###   ########.fr       */
+/*   Updated: 2023/11/02 12:24:33 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <stdio.h>
+// #include <CUnit/CUnit.h>
 #include <fcntl.h>
+// #include <unistd.h>
+#include "get_next_line.h"
 
-int	main(void)
+// void test_get_next_line_empty_file(void) {
+//     int fd = open("emptyfile.txt", O_RDONLY);
+//     CU_ASSERT_EQUAL(get_next_line(fd), -1);
+//     close(fd);
+// }
+
+// void test_get_next_line_buffer_size_zero(void) {
+//     int fd = open("testfile.txt", O_RDONLY);
+//     BUFFER_SIZE = 0;
+//     CU_ASSERT_EQUAL(get_next_line(fd), -1);
+//     close(fd);
+// }
+
+// void test_get_next_line_negative_fd(void) {
+//     int fd = -1;
+//     CU_ASSERT_EQUAL(get_next_line(fd), -1);
+// }
+
+// int main() {
+//     CU_initialize_registry();
+
+//     CU_pSuite suite1 = CU_add_suite("get_next_line_test_suite", 0, 0);
+
+//     CU_add_test(suite1, "test_get_next_line_valid_file", test_get_next_line_valid_file);
+//     CU_add_test(suite1, "test_get_next_line_invalid_file", test_get_next_line_invalid_file);
+//     CU_add_test(suite1, "test_get_next_line_empty_file", test_get_next_line_empty_file);
+//     CU_add_test(suite1, "test_get_next_line_buffer_size_zero", test_get_next_line_buffer_size_zero);
+//     CU_add_test(suite1, "test_get_next_line_negative_fd", test_get_next_line_negative_fd);
+
+//     CU_basic_set_mode(CU_BRM_VERBOSE);
+//     CU_basic_run_tests();
+//     CU_cleanup_registry();
+
+//     return CU_get_error();
+// }
+
+#include <stdio.h>
+int main()
 {
-	int fd = open("texttest.txt", O_RDONLY);
+    int fd = open("texttest.txt", O_RDONLY);
 	for (int i = 0; i < 14; i++)
 	{
-		char *s = get_next_line(fd);
-		printf("%s", s);
-		free(s);
+		printf("%s", get_next_line(fd));
 	}
-		
-	return (0);
+	close(fd);
 }
